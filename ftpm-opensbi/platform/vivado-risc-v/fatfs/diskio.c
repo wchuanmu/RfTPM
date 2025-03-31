264d6d04a7dfcfa01c0b7bbe6a191bc6a09a39f8
ssfx <1250265945@qq.com>
2025-03-31 23:55:09 +0800

    add diskio handle func

--- ftpm-opensbi/platform/vivado-risc-v/fatfs/diskio.c
+++ ftpm-opensbi/platform/vivado-risc-v/fatfs/diskio.c
@@ -457,3 +457,221 @@ static int ini_sd(void) {
     drv_status &= ~STA_NOINIT;
     return 0;
 }
+
+
+
+DSTATUS disk_initialize (
+	BYTE pdrv				/* Physical drive nmuber to identify the drive */
+)
+{
+	// DSTATUS stat;
+	// int result;
+
+	// switch (pdrv) {
+	// case DEV_RAM :
+	// 	result = RAM_disk_initialize();
+
+	// 	// translate the reslut code here
+
+	// 	return stat;
+
+	// case DEV_MMC :
+	// 	result = MMC_disk_initialize();
+
+	// 	// translate the reslut code here
+
+	// 	return stat;
+
+	// case DEV_USB :
+	// 	result = USB_disk_initialize();
+
+	// 	// translate the reslut code here
+
+	// 	return stat;
+	// }
+	// return STA_NOINIT;
+
+    if (ini_sd() < 0) sbi_printf("Cannot access SD: %s\n", errno_to_str());
+    return drv_status;
+}
+
+DSTATUS disk_status(BYTE drv) {
+    return drv_status;
+}
+
+
+
+/*-----------------------------------------------------------------------*/
+/* Read Sector(s)                                                        */
+/*-----------------------------------------------------------------------*/
+
+DRESULT disk_read (
+	BYTE pdrv,		/* Physical drive nmuber to identify the drive */
+	BYTE *buff,		/* Data buffer to store read data */
+	LBA_t sector,	/* Start sector in LBA */
+	UINT count		/* Number of sectors to read */
+)
+{
+	// DRESULT res;
+	// int result;
+
+	// switch (pdrv) {
+	// case DEV_RAM :
+	// 	// translate the arguments here
+
+	// 	result = RAM_disk_read(buff, sector, count);
+
+	// 	// translate the reslut code here
+
+	// 	return res;
+
+	// case DEV_MMC :
+	// 	// translate the arguments here
+
+	// 	result = MMC_disk_read(buff, sector, count);
+
+	// 	// translate the reslut code here
+
+	// 	return res;
+
+	// case DEV_USB :
+	// 	// translate the arguments here
+
+	// 	result = USB_disk_read(buff, sector, count);
+
+	// 	// translate the reslut code here
+
+	// 	return res;
+	// }
+
+	// return RES_PARERR;
+
+    if (!count) return RES_PARERR;
+    if (drv_status & STA_NOINIT) return RES_NOTRDY;
+
+    // sbi_printf("[fTPM] disk_read(): buff=%p, sector=%x, count=%x\n", buff, sector, count);
+    /* Convert LBA to byte address if needed */
+    if (!(card_type & CT_BLOCK)) sector *= 512;
+    while (count > 0) {
+        UINT bcnt = count > MAX_BLOCK_CNT ? MAX_BLOCK_CNT : count;
+        unsigned bytes = bcnt * 512;
+        if (send_data_cmd_r(bcnt == 1 ? CMD17 : CMD18, sector, buff, bcnt) < 0) return RES_ERROR;
+        if (bcnt > 1 && send_cmd(CMD12, 0) < 0) return RES_ERROR;
+        sector += (card_type & CT_BLOCK) ? bcnt : bytes;
+        count -= bcnt;
+        buff += bytes;
+    }
+
+    return RES_OK;
+}
+
+
+
+/*-----------------------------------------------------------------------*/
+/* Write Sector(s)                                                       */
+/*-----------------------------------------------------------------------*/
+
+#if FF_FS_READONLY == 0
+
+DRESULT disk_write (
+	BYTE pdrv,			/* Physical drive nmuber to identify the drive */
+	const BYTE *buff,	/* Data to be written */
+	LBA_t sector,		/* Start sector in LBA */
+	UINT count			/* Number of sectors to write */
+)
+{
+	// DRESULT res;
+	// int result;
+
+	// switch (pdrv) {
+	// case DEV_RAM :
+	// 	// translate the arguments here
+
+	// 	result = RAM_disk_write(buff, sector, count);
+
+	// 	// translate the reslut code here
+
+	// 	return res;
+
+	// case DEV_MMC :
+	// 	// translate the arguments here
+
+	// 	result = MMC_disk_write(buff, sector, count);
+
+	// 	// translate the reslut code here
+
+	// 	return res;
+
+	// case DEV_USB :
+	// 	// translate the arguments here
+
+	// 	result = USB_disk_write(buff, sector, count);
+
+	// 	// translate the reslut code here
+
+	// 	return res;
+	// }
+
+	// return RES_PARERR;
+
+    if (!count) return RES_PARERR;
+    if (drv_status & STA_NOINIT) return RES_NOTRDY;
+
+    // sbi_printf("[fTPM] disk_write(): buff=%p, sector=%x, count=%x\n", buff, sector, count);
+
+    /* Convert LBA to byte address if needed */
+    if (!(card_type & CT_BLOCK)) sector *= 512;
+    for (UINT i = 0; i < count; ++i) {
+        UINT bcnt = count > MAX_BLOCK_CNT ? MAX_BLOCK_CNT : count;
+        unsigned bytes = bcnt * 512;
+        if (send_data_cmd_w(bcnt == 1 ? CMD24 : CMD25, sector, buff, bcnt) < 0) return RES_ERROR;
+        if (bcnt > 1 && send_cmd(CMD12, 0) < 0) return RES_ERROR;
+        sector += (card_type & CT_BLOCK) ? bcnt : bytes;
+        count -= bcnt;
+        buff += bytes;
+    }
+
+    return RES_OK; // Successfully written
+}
+
+#endif
+
+
+/*-----------------------------------------------------------------------*/
+/* Miscellaneous Functions                                               */
+/*-----------------------------------------------------------------------*/
+
+DRESULT disk_ioctl (
+	BYTE pdrv,		/* Physical drive nmuber (0..) */
+	BYTE cmd,		/* Control code */
+	void *buff		/* Buffer to send/receive control data */
+)
+{
+	DRESULT res;
+	int result;
+
+    sbi_printf("[FATFS] disk_ioctl(): cmd=%x\n", cmd);
+
+	switch (pdrv) {
+	case DEV_RAM :
+
+		// Process of the command for the RAM drive
+
+		return res;
+
+	case DEV_MMC :
+
+		// Process of the command for the MMC/SD card
+
+		return res;
+
+	case DEV_USB :
+
+		// Process of the command the USB drive
+
+		return res;
+	}
+
+	return RES_PARERR;
+}
+
